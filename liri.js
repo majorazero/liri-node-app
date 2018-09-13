11d866a43b1a044ad941f61f4c2e89cfc5994551
majorazero <daniel.cw.hsu@gmail.com>
2018-09-13 16:57:50 -0700

    implemented movie-this and dealt with non stringed inputs

--- liri.js
+++ liri.js
@@ -3,10 +3,18 @@ let dotenv = require("dotenv").config();
 let request = require("request");
 let moment = require("moment");
 let command = process.argv[2];
+let arg = "";
+
+for(let i = 3; i < process.argv.length; i++){
+  arg += process.argv[i];
+  if(i !== process.argv.length-1){
+    arg += "+";
+  }
+}
 
 switch(command){
   case "concert-this":
-    request("https://rest.bandsintown.com/artists/"+process.argv[3]+"/events?app_id=codingbootcamp",function(error,response,body){
+    request("https://rest.bandsintown.com/artists/"+arg+"/events?app_id=codingbootcamp",function(error,response,body){
       if(error){
         console.log(error);
       }
@@ -20,11 +28,37 @@ switch(command){
           console.log("/////////");
         }
       }
-    })
+    });
     break;
   case "spotify-this-song":
     break;
   case "movie-this":
+      if(process.argv[3] === undefined){
+        arg = "Mr. Nobody";
+      }
+      request("http://www.omdbapi.com/?t="+arg+"&y=&plot=short&apikey=trilogy",function(error,response,body){
+        let rotScore = "N/A";
+        if (error){
+          console.log(error);
+        }
+        else {
+          let movieData = JSON.parse(body);
+          console.log("/////////");
+          console.log("Title: "+movieData.Title);
+          console.log("Year: "+movieData.Year);
+          console.log("IMDB Rating: "+movieData.imdbRating);
+          for(let i = 0; i < movieData.Ratings.length; i++){
+            if(movieData.Ratings[i].Source === "Rotten Tomatoes"){
+              rotScore = movieData.Ratings[i].Value;
+            }
+          }
+          console.log("Rotten Tomatoe Rating: "+rotScore);
+          console.log("Country: "+movieData.Country);
+          console.log("Plot: "+movieData.Plot);
+          console.log("Actors: "+movieData.Actors);
+          console.log("/////////");
+        }
+      });
     break;
   case "do-what-it-says":
     break;
